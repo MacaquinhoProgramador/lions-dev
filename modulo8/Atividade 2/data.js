9ef8a21e9946eff9e1f5856306c758ff22f2205a
MacaquinhoProgramador <guilhermeszumski@gmail.com>
2024-09-09 17:42:51 -0300

    Atividade parte do guizeira

---
+++ modulo8/Atividade 2/data.js
@@ -0,0 +1,10 @@
+let produto = [
+    {
+        "id": 1,
+        "nome": "Nome do Produto",
+        "categoria": "Eletrônicos",
+        "preco": 1000,
+        "estoque": 50
+    }]
+
+module.exports = { produto } 
---
+++ modulo8/Atividade 2/produtos/get.js
@@ -0,0 +1,7 @@
+const { produto } = require('../data');
+
+const listarProdutos = (req, res) => {
+    res.status(200).send(produto);
+}
+
+module.exports = listarProdutos;
