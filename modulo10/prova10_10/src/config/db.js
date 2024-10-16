afa8355a6b405a67d87ac51b8a0276034f7091f2
MacaquinhoProgramador <guilhermeszumski@gmail.com>
2024-10-16 16:45:20 -0300

    coisa

--- modulo10/prova10_10/src/config/db.js
+++ modulo10/prova10_10/src/config/db.js
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 const connectDB = async() => {
     mongoose
-        .connect('mongodb://localhost:27017/workshop')
+        .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}`)
         .then(() => console.log('Connected to MongoDB'))
         .catch((error) => console.log(error));
 };
